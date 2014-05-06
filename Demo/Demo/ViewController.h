4212fdf9ed2687e6e5ee37b382808478d458f604
Mao Nishi <nishi.nds@gmail.com>
2014-05-06 22:51:12 +0900

    デモ用のコード作成

---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Mao Nishi on 2014/05/06.
+//  Copyright (c) 2014年 Mao Nishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by Mao Nishi on 2014/05/06.
+//  Copyright (c) 2014年 Mao Nishi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
